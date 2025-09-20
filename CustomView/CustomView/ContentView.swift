d57e8b47ecd45832e6eab274842e26a578f8876e
Md Rashid Iqubal <rashid11931@gmail.com>
2025-09-20 13:22:20 +0530

    CustomView Intial Commit

---
+++ CustomView/CustomView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomView
+//
+//  Created by MD RASHID IQUBAL on 19/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomView/CustomView/CustomViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomViewApp.swift
+//  CustomView
+//
+//  Created by MD RASHID IQUBAL on 19/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
